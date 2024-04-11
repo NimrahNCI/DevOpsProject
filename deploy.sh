fb01b86c862f7a39092aaf7d6ff3e81d69540ac6
Nimrah12 <nimrahmemon12@gmail.com>
2024-04-11 23:31:54 +0100

    added deploy.sh and edit config file

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# Update system packages and install Node.js and npm
+sudo apt update && sudo apt install nodejs npm
+
+# Install pm2 globally
+sudo npm install -g pm2
+
+# Stop the pm2 process named "example_app"
+pm2 stop example_app
+
+# Navigate to the ExampleApplication directory
+cd DevOpsProject/
+
+# Install npm dependencies
+npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
+
+# Start the pm2 process for your application
+pm2 start ./bin/www --name example_app
